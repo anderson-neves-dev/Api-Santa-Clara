07aee6d4a03f0594d4d1a3db430004af45cfcb80
Lauane Oliveira <oliveiralauane91@gmail.com>
2024-11-04 21:57:30 -0300

    feat: added new paciente atributes

--- src/paciente/dto/create-paciente.dto.ts
+++ src/paciente/dto/create-paciente.dto.ts
@@ -23,32 +23,79 @@ export class CreatePacienteDTO {
     console.log(document);
     if (document.length === 11) {
       if (!cpf.isValid(document)) {
-        throw new UnprocessableEntityException('O CPF informado é inválido.');
+        throw new UnprocessableEntityException('The CPF is invalid.');
       }
     } else {
       throw new UnprocessableEntityException(
-        'O documento informado não é um CPF válido.',
+        'The document provided is not a valid CPF .',
       );
     }
 
-    return 'ola';
+    return document;
   })
   cpf: string;
 
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
-  @MaxLength(100)
-  nome: string;
+  name: string;
 
   @ApiProperty()
   @IsNotEmpty()
   @Transform(({ value }) => new Date(value), { toClassOnly: true }) // Transformar string para Date
   @IsDate()
-  data_nascimento: Date;
+  dateBirthday: Date;
 
   @ApiProperty({ example: 'jon@gmail.com' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(11)
+  phoneNumber: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  cep: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)  
+  street: string;
+  
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(20)
+  number: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  complement: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  neighborhood: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  city: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(2)
+  state: string;
 }
--- src/paciente/entities/paciente.entity.ts
+++ src/paciente/entities/paciente.entity.ts
@@ -15,13 +15,37 @@ export class Paciente {
     type: 'varchar',
     length: 100,
   })
-  nome: string;
+  name: string;
 
   @Column({
     type: 'date',
   })
-  data_nascimento: Date;
+  dateBirthday: Date;
 
   @Column({ type: 'varchar', length: 100 })
   email: string;
+
+  @Column({ type: 'varchar', length: 11 })
+  phoneNumber: string;
+
+  @Column({ type: 'char', length: 8 })
+  cep: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  street: string;
+  
+  @Column({ type: 'varchar', length: 20 })
+  number: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  complement: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  neighborhood: string;
+
+  @Column({ type: 'varchar', length: 100 }) 
+  city: string;
+
+  @Column({ type: 'varchar', length: 2 })
+  state: string;
 }
